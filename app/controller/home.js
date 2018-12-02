e9d5f23811e5d143ff0d51821d144c6cdb128b08
zhongzhenga <616028858@qq.com>
2018-12-02 22:09:24 +0800

    修改home controller/home.js

--- app/controller/home.js
+++ app/controller/home.js
@@ -6,35 +6,6 @@ class HomeController extends Controller {
     async index() {
         this.ctx.body = 'hi, egg';
     }
-
-    async login() {
-        const ctx = this.ctx;
-        const params = ctx.request.body;
-        const user = await ctx.service.home.login(params);
-        if(user){
-            ctx.body = {
-                success: true,
-                backData: user
-            };
-        }else {
-            ctx.body = {
-                success: false,
-                backMsg: "用户编码或密码不正确！"
-            };
-        }
-    }
-
-    async queryList() {
-        const ctx = this.ctx;
-        const adminList = await ctx.service.admin.queryList();
-        ctx.body = adminList;
-    }
-
-    async add() {
-        const ctx = this.ctx;
-        const result = await ctx.service.admin.add();
-        ctx.body = result;
-    }
 }
 
 module.exports = HomeController;
