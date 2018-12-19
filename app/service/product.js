7f3100dcf0ed61a39c4a6677628747cca5be7ac4
zhongzhenga <616028858@qq.com>
2018-12-19 23:51:04 +0800

    修改产品service；

--- app/service/product.js
+++ app/service/product.js
@@ -5,108 +5,110 @@ const UUID = require('uuid');
 
 class ProductService extends Service {
 
-  async queryList(params) {
-    const ctx = this.ctx;
-    const Product = ctx.model.Product;
-    const ProductCategory = ctx.model.ProductCategory;
-    Product.belongsTo(ProductCategory, { foreignKey: 'productCategoryId', targetKey: 'productCategoryCode' })
-    const { pageNumber, pageSize } = params;
-    const total = await Product.findAll({
-      where: {}
-    });
-    const res = await Product.findAll({
-      where: {},
-      include: [{
-        model: ProductCategory,
-        attributes: ['productCategoryName']
-      }],
-      order: [
-        ['created_at', 'DESC']
-      ], // 排序方式
-      limit: pageSize, // 返回数据量
-      offset: (pageNumber - 1) * pageSize, // 数据偏移量
-    });
+    async queryList(params) {
+        const ctx = this.ctx;
+        const Sequelize = this.app.Sequelize;
+        const Product = ctx.model.Product;
+        const ProductCategory = ctx.model.ProductCategory;
+        Product.belongsTo(ProductCategory, {foreignKey: 'productCategoryId'});
+        const {pageNumber, pageSize} = params;
+        const total = await Product.findAll({
+            where: {}
+        });
+        const res = await Product.findAll({
+            attributes: [
+                'id',
+                'shopId',
+                [Sequelize.col('ProductCategory.product_category_name'), 'productCategoryName'],
+                'productCode',
+                'productName',
+            ],
+            include: [{
+                model: ProductCategory,
+                attributes: []
+            }],
+            order: [
+                ['created_at', 'DESC']
+            ],
+            limit: pageSize,
+            offset: (pageNumber - 1) * pageSize,
+        });
 
-    // for (let item of res) {
-    //   item.productCategoryName = item.ProductCategory.productCategoryName
-    // }
-    // console.log('res ===', res)
+        return {
+            content: res,
+            totalElements: total.length
+        };
+    }
 
-    return {
-      content: res,
-      totalElements: total.length
-    };
-  }
+    async queryDetail(params) {
+        const ctx = this.ctx;
+        const {id} = params;
+        const res = await ctx.model.Product.findById(id);
 
-  async queryDetail(params) {
-    const ctx = this.ctx;
-    const { id } = params;
-    const res = await ctx.model.Product.findById(id);
+        return res;
+    }
 
-    return res;
-  }
+    async add(fieldsValue) {
+        const ctx = this.ctx;
+        const row = {
+            // id: UUID.v1(),
+            ...fieldsValue
+        };
+        const res = await ctx.model.Product.create(row);
 
-  async add(fieldsValue) {
-    const ctx = this.ctx;
-    const row = {
-      // id: UUID.v1(),
-      ...fieldsValue
-    };
-    const res = await ctx.model.Product.create(row);
+        return {
+            rowsAffected: res,
+        };
+    }
 
-    return {
-      rowsAffected: res,
-    };
-  }
+    async update(fieldsValue) {
+        const ctx = this.ctx;
+        const {id, ...restFieldsValue} = fieldsValue;
+        const res = await ctx.model.Product.update(restFieldsValue, {
+            where: {id}
+        });
 
-  async update(fieldsValue) {
-    const ctx = this.ctx;
-    const { id, ...restFieldsValue } = fieldsValue;
-    const res = await ctx.model.Product.update(restFieldsValue, {
-      where: { id }
-    });
+        return {rowsAffected: res};
+    }
 
-    return { rowsAffected: res };
-  }
+    //查询所有分类
+    async queryAllCategoryList() {
+        const ctx = this.ctx;
+        const total = await ctx.model.ProductCategory.findAll();
+        return total
+    }
 
-  //查询所有分类
-  async queryAllCategoryList() {
-    const ctx = this.ctx;
-    const total = await ctx.model.ProductCategory.findAll();
-    return total
-  }
+    //产品类别
+    async queryCategoryList(params) {
+        const ctx = this.ctx;
+        const {pageNumber, pageSize} = params;
+        const total = await ctx.model.ProductCategory.findAll({
+            where: {}
+        });
+        const res = await ctx.model.ProductCategory.findAll({
+            where: {},
+            order: [
+                ['created_at', 'desc']
+            ], // 排序方式
+            limit: pageSize, // 返回数据量
+            offset: (pageNumber - 1) * pageSize, // 数据偏移量
+        });
 
-  //产品类别
-  async queryCategoryList(params) {
-    const ctx = this.ctx;
-    const { pageNumber, pageSize } = params;
-    const total = await ctx.model.ProductCategory.findAll({
-      where: {}
-    });
-    const res = await ctx.model.ProductCategory.findAll({
-      where: {},
-      order: [
-        ['created_at', 'desc']
-      ], // 排序方式
-      limit: pageSize, // 返回数据量
-      offset: (pageNumber - 1) * pageSize, // 数据偏移量
-    });
+        return {
+            content: res,
+            totalElements: total.length
+        };
+    }
 
-    return {
-      content: res,
-      totalElements: total.length
-    };
-  }
+    async categoryAdd(row) {
+        const ctx = this.ctx;
+        const res = await ctx.model.ProductCategory.findOrCreate({
+            where: {productCategoryName: row.productCategoryName},
+            defaults: {...row}
+        });
 
-  async categoryAdd(row) {
-    const ctx = this.ctx;
-    const res = await ctx.model.ProductCategory.findOrCreate({
-      where: { productCategoryName: row.productCategoryName },
-      defaults: { ...row }
-    });
-
-    return res;
-  }
+        return res;
+    }
 }
 
 module.exports = ProductService;
