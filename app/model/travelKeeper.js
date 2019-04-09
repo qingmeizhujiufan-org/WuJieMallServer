3087dfad73840824b5d398cc925993eb9cecf875
weiwang <2424947384@qq.com>
2019-04-09 21:53:23 +0800

    添加旅游商家认证、审核后台接口

---
+++ app/model/travelKeeper.js
@@ -0,0 +1,97 @@
+'use strict';
+
+const Moment = require('moment');
+
+module.exports = app => {
+    const {UUIDV1, INTEGER, STRING, DECIMAL, DATE, TEXT} = app.Sequelize;
+    const TravelKeeper = app.model.define('TravelKeeper', {
+        /* 旅游商家ID */
+        id: {
+            type: UUIDV1,
+            primaryKey: true,
+            field: 'id'
+        },
+        /* 旅游商家缩略图 */
+        thumbnail: {
+            type: STRING(255),
+            field: 'thumbnail'
+        },
+        /* 旅游商家图 */
+        headerPic: {
+            type: STRING(255),
+            field: 'header_pic'
+        },
+        /* 旅游商家详情图 */
+        detailPic: {
+            type: STRING(255),
+            field: 'detail_pic'
+        },
+        /* 旅游商家名称 */
+        travelKeeperName: {
+            type: STRING(255),
+            field: 'travel_keeper_name'
+        },
+        /* 咨询电话 */
+        telephone: {
+            type: STRING(32),
+            field: 'travel_keeper_telephone'
+        },
+        /* 固定电话 */
+        phone: {
+            type: STRING(32),
+            field: 'travel_keeper_phone'
+        },
+        /* 地址 */
+        travelKeeperAddress: {
+            type: STRING(255),
+            field: 'travel_keeper_address'
+        },
+        // 备注
+        mark: {
+            type: STRING(255),
+            field: 'mark'
+        },
+        /* 营业状态 */
+        businessStatus: {
+          type: INTEGER,
+          field: 'business_status'
+        },
+        /* 营业状态明文 */
+        businessStatusText: {
+          type: STRING(255),
+          field: 'business_status_text'
+        },
+          /* 审核状态 */
+        checkStatus: {
+          type: INTEGER,
+          field: 'check_status'
+        },
+        /* 修改人 */
+        updateBy: {
+            type: STRING(255),
+            field: 'update_by'
+        },
+        /* 创建人 */
+        createBy: {
+            type: STRING(255),
+            field: 'create_by'
+        },
+        created_at: {
+            type: DATE,
+            get() {
+                return this.getDataValue('created_at') && Moment(this.getDataValue('created_at')).format('YYYY-MM-DD HH:mm:ss');
+            }
+        },
+        updated_at: {
+            type: DATE,
+            get() {
+                return this.getDataValue('updated_at') && Moment(this.getDataValue('updated_at')).format('YYYY-MM-DD HH:mm:ss');
+            }
+        }
+    }, {
+        freezeTableName: true,
+        tableName: 'travel_keeper_info',
+        timestamps: true,
+    });
+    return TravelKeeper;
+};
---
+++ app/service/travelKeeper.js
@@ -0,0 +1,202 @@
+'use strict';
+
+const Service = require('egg').Service;
+
+class TravelKeeperService extends Service {
+  async queryList(params) {
+    const ctx = this.ctx;
+    const Sequelize = this.app.Sequelize;
+    const TravelKeeper = ctx.model.TravelKeeper;
+    const Attachment = ctx.model.Attachment;
+    TravelKeeper.belongsTo(Attachment, { foreignKey: 'thumbnail' });
+    const { pageNumber = 1, pageSize = 10, keyWords = '' } = params;
+    const whereCondition = {
+      '$or': {
+        travelKeeperName: {
+          '$like': '%' + keyWords + '%'
+        },
+      }
+    };
+
+    const dataList = await Promise.all([
+      TravelKeeper.findAll({
+        where: whereCondition,
+      }),
+      TravelKeeper.findAll({
+        where: whereCondition,
+        attributes: [
+          'id',
+          'headerPic',
+          'detailPic',
+          'travelKeeperName',
+          'telephone',
+          'phone',
+          'travelKeeperAddress',
+          'mark',
+          'businessStatus',
+          'businessStatusText',
+          'checkStatus',
+          'updateBy',
+          'createBy',
+          'updated_at',
+          'created_at'
+        ],
+        include: [{
+          model: Attachment,
+          attributes: ['id', 'fileType']
+        }],
+        order: [
+          ['created_at', 'DESC']
+        ],
+        limit: pageSize,
+        offset: (pageNumber - 1) * pageSize,
+      })
+    ]);
+
+    return {
+      content: dataList[1],
+      pageNumber,
+      pageSize,
+      totalElements: dataList[0].length,
+      totalPages: Math.ceil(dataList[0].length / pageSize)
+    };
+  }
+
+  async queryMobileList(params) {
+    const ctx = this.ctx;
+    const Sequelize = this.app.Sequelize;
+    const TravelKeeper = ctx.model.TravelKeeper;
+    const Attachment = ctx.model.Attachment;
+    TravelKeeper.belongsTo(Attachment, { foreignKey: 'thumbnail' });
+    const { hotelId } = params;
+
+    const dataList = await TravelKeeper.findAll({
+      where: {
+        hotelId,
+        state: 2
+      },
+      attributes: [
+        'id',
+        'headerPic',
+        'detailPic',
+        'travelKeeperName',
+        'telephone',
+        'phone',
+        'travelKeeperAddress',
+        'mark',
+        'businessStatus',
+        'businessStatusText',
+        'checkStatus',
+        'updateBy',
+        'createBy',
+        'updated_at',
+        'created_at'
+      ],
+      include: [{
+        model: Attachment,
+        attributes: ['id', 'fileType']
+      }],
+      order: [
+        ['created_at', 'DESC']
+      ]
+    });
+
+    return dataList;
+  }
+
+  async queryListTop3() {
+    const ctx = this.ctx;
+    const TravelKeeper = ctx.model.TravelKeeper;
+    const Attachment = ctx.model.Attachment;
+    TravelKeeper.belongsTo(Attachment, { foreignKey: 'thumbnail' });
+
+    const dataList = await TravelKeeper.findAll({
+      attributes: [
+        'id',
+        'thumbnail',
+        'TravelKeeperLastTime',
+        'TravelKeeperHas',
+        'TravelKeeperLimiteNumber',
+        'TravelKeeperBeginTime',
+        'TravelKeeperEndTime',
+        'manPrice',
+        'TravelKeeperFrom',
+        'TravelKeeperTo',
+        'TravelKeeperUsecar',
+        'linePlay',
+        'TravelKeeperDesc',
+        'state',
+        'updateBy',
+        'createBy',
+        'updated_at',
+        'created_at',
+      ],
+      include: [{
+        model: Attachment,
+        attributes: ['id', 'fileType']
+      }],
+      order: [
+        ['created_at', 'DESC']
+      ],
+      limit: 3
+    });
+
+    return {
+      content: dataList,
+    };
+  }
+
+  async queryDetail(params) {
+    const ctx = this.ctx;
+    const TravelKeeper = ctx.model.TravelKeeper;
+    const { id } = params;
+    const res = await ctx.model.TravelKeeper.findOne({
+      where: {
+        id
+      }
+    });
+
+    return res;
+  }
+
+  async add(fieldsValue) {
+    const ctx = this.ctx;
+    const row = {
+      ...fieldsValue
+    };
+    const res = await ctx.model.TravelKeeper.create(row);
+
+    return {
+      rowsAffected: res,
+    };
+  }
+
+  async update(fieldsValue) {
+    const ctx = this.ctx;
+    const { id, ...restFieldsValue } = fieldsValue;
+    const res = await ctx.model.TravelKeeper.update(restFieldsValue, {
+      where: { id }
+    });
+
+    return { rowsAffected: res };
+  }
+
+  async delete(params) {
+    const res = await this.ctx.model.TravelKeeper.destroy({
+      where: { id: params.id }
+    });
+    return res;
+  }
+
+  async check(fieldsValue) {
+    const ctx = this.ctx;
+    const { id, ...restFieldsValue } = fieldsValue;
+    const res = await ctx.model.TravelKeeper.update(restFieldsValue, {
+      where: { id }
+    });
+
+    return { rowsAffected: res };
+  }
+}
+
+module.exports = TravelKeeperService;
